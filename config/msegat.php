65332f1fe1f724df10e78dbebb969a7609c496ac
Abd Almnum Mahmoud <30375406+Abdalmnum@users.noreply.github.com>
2023-02-24 23:27:46 +0300

    Update msegat.php

--- config/msegat.php
+++ config/msegat.php
@@ -2,8 +2,8 @@
 
 return [
     "msegat_url"=>env('MSEGAT_URL','https://www.msegat.com/gw/sendsms.php'),
-    "userName"=>env('MSEGAT_USERNAME','jawahra2020'),
-    "apiKey"=>env('MSEGAT_API_KEY','c166e3a382e3a1da8a4f7040b6e48f27'),
-    'userSender'=>env('MSEGAT_SENDER','Aljawhara'),
+    "userName"=>env('MSEGAT_USERNAME',''),
+    "apiKey"=>env('MSEGAT_API_KEY',''),
+    'userSender'=>env('MSEGAT_SENDER',''),
     'msgEncoding'=>env('MSEGAT_ENCODING')
 ];
