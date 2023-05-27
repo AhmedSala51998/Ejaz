f008571faf7a395078ca7bd7fb2566b0a2dfcc8e
Monem <abdalmnum4477@gmail.com>
2023-05-27 16:08:28 +0300

    last_v

--- config/msegat.php
+++ config/msegat.php
@@ -1,9 +1,5 @@
 <?php
 
 return [
-    "msegat_url"=>env('MSEGAT_URL','https://www.msegat.com/gw/sendsms.php'),
-    "userName"=>env('MSEGAT_USERNAME','jawahra2020'),
-    "apiKey"=>env('MSEGAT_API_KEY','c166e3a382e3a1da8a4f7040b6e48f27'),
-    'userSender'=>env('MSEGAT_SENDER','Aljawhara'),
-    'msgEncoding'=>env('MSEGAT_ENCODING')
+
 ];
