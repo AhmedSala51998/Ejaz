88dd96fcc2ea817586960908ac5c0ed78bbd33f2
mayyoussri93 <mayyoussri93@gmail.com>
2023-07-16 12:38:45 -0500

    live

--- app/Http/Controllers/Admin/CRUD/AdminNotesController.php
+++ app/Http/Controllers/Admin/CRUD/AdminNotesController.php
@@ -95,43 +95,45 @@ class AdminNotesController extends Controller
             /* 'desc'=>'required|array',
              'desc.*'=>'required',*/
         ]);
+                $data = $request->except('status','id');
+
         $data['note'] = $request->note;
         $data['order_id'] = $id;
-        $data = $request->except('id','status');
         /*  $data ['image'] = $this->uploadFiles('our_services',$request->file('image'),null );*/
         Notes::create($data);
-        $params=array(
-            'token' => '4swdwztymusjggiv',
-            'to' => '+201278295433',
-            'body' => $request->note
-        );
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.ultramsg.com/instance54417/messages/chat'",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_SSL_VERIFYHOST => 0,
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => http_build_query($params),
-            CURLOPT_HTTPHEADER => array(
-                "content-type: application/x-www-form-urlencoded"
-            ),
-        ));
+   $params=array(
+'token' => '4swdwztymusjggiv',
+'to' => '+201278295433',
+'body' => $request->note
+);
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.ultramsg.com/instance54417/messages/chat",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_SSL_VERIFYHOST => 0,
+  CURLOPT_SSL_VERIFYPEER => 0,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => http_build_query($params),
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/x-www-form-urlencoded"
+  ),
+));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-        curl_close($curl);
+curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+      
         return response()->json(1, 200);
 
     }
