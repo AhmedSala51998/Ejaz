f1cdfdbf4c613b7965a34557718767ee6d5a4866
git <ahmedsalah511998@gmail.com>
2025-07-12 23:40:09 +0300

    rrrr

--- resources/views/frontend/pages/profile/parts/ordersHistory.blade.php
+++ resources/views/frontend/pages/profile/parts/ordersHistory.blade.php
@@ -8,7 +8,7 @@
     <div style="{{$ordersHistory->currentPage() == $ordersHistory->lastPage() ?"display:none!important":""}}" class="d-flex align-items-center justify-content-center py-5 row" >
 
 
-        <button id="load_more_history_orders_button" class="animatedLink">
+        <button id="load_more_history_orders_button" class="animatedLinkk loadMoreBtn">
                 {{__('frontend.load more')}}
                     <i class="fa-regular fa-left-long ms-2"><span></span></i>
         </button>
@@ -22,4 +22,21 @@
         <img style="width: 500px;height: 500px ; object-fit: contain;" src="{{asset('frontend/img/no-order.png')}}" alt="no data for current orders">
     </div>
 @endif
-
+<style>
+    /* زر تحميل المزيد */
+    .loadMoreBtn {
+        background: transparent;
+        border: 2px solid #f4a835;
+        color: #f4a835;
+        padding: 10px 25px;
+        font-weight: bold;
+        border-radius: 30px;
+        transition: 0.3s ease;
+        font-size: 16px;
+    }
+    .loadMoreBtn:hover {
+        background-color: #f4a835;
+        color: #fff;
+        transform: scale(1.03);
+    }
+</style>
